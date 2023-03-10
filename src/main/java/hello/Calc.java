5e9457a59a8fdf9aaf0b52f2b804973075c2f9c8
yohan <sjohn9227@naver.com>
2023-03-10 16:44:24 +0900

    + - 여러개 구현

--- src/main/java/hello/Calc.java
+++ src/main/java/hello/Calc.java
@@ -6,36 +6,15 @@ public class Calc {
         boolean needPlus = exp.contains("+");
         boolean needMinus = exp.contains("-");
 
+        exp = exp.replaceAll("- ", "+ -");
 
-        String[] split = null;
-        if (needPlus) {
-             split = exp.split(" \\+ ");
-        } else if (needMinus) {
-             split = exp.split(" \\- ");
+        String[] split = exp.split(" \\+ ");
+
+        int ans = 0;
+        for (String s : split) {
+            ans += Integer.parseInt(s);
         }
 
-        if (split.length > 2) {
-            int c = Integer.parseInt(split[2]);
-        }
-        int first = Integer.parseInt(split[0]);
-        int second = Integer.parseInt(split[1]);
-        int third =0;
-
-        if (split.length > 2) {
-             third = Integer.parseInt(split[2]);
-        }
-
-        if (needPlus) {
-            return first + second + third;
-        } else if (needMinus) {
-            return first -  second + third;
-        }
-
-
-        throw new RuntimeException("해석할 수 없습니다.");
+        return ans;
     }
-
-
-
-
 }
