2d46edf2259dd47e78fe5dde05dbbe90fc769917
yohan <sjohn9227@naver.com>
2023-03-10 16:48:50 +0900

    * 구현

--- src/main/java/hello/Calc.java
+++ src/main/java/hello/Calc.java
@@ -3,16 +3,26 @@ package hello;
 public class Calc {
     public static int run(String exp) {
 
-        boolean needPlus = exp.contains("+");
-        boolean needMinus = exp.contains("-");
-
-        exp = exp.replaceAll("- ", "+ -");
-
-        String[] split = exp.split(" \\+ ");
-
+        boolean needMulti = exp.contains("*");
+        boolean needPlus = !needMulti;
         int ans = 0;
-        for (String s : split) {
-            ans += Integer.parseInt(s);
+
+        if (needPlus) {
+            exp = exp.replaceAll("- ", "+ -");
+
+            String[] split = exp.split(" \\+ ");
+
+            for (String s : split) {
+                ans += Integer.parseInt(s);
+            }
+        } else {
+            ans = 1;
+            String[] split = exp.split(" \\* ");
+
+            for (String s : split) {
+                ans *= Integer.parseInt(s);
+            }
+
         }
 
         return ans;
